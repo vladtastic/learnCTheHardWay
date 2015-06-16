7fa89bdc41d29db9e0e331b3eecd34740a46253f
Vladimir Coxall <vladimir.coxall@gmail.com>
2015-06-16 10:10:06 -0500

    Exercise 5

---
+++ ex5/ex1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	puts("Hello world.");
+
+	puts("Is this a more complex string?");
+
+	puts("In vino veritas");
+
+	puts("?! - I'm sorry I can't do that HAL");
+
+	puts("Bite my shiny metal...");
+
+	return 0;
+}
